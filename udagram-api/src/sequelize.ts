289549a71ce56869e209a81dcd01230616942327
MotazShafik <Motaz.Shafik@hotmail.com>
2022-02-05 04:30:04 +0400

    apps are running and connected to RDS , S3

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,17 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
+export const sequelize = new Sequelize(
+	'postgres://postgres:postgres@database-1.c69skcch8xos.us-east-1.rds.amazonaws.com:5432/postgres'
+);
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+// export const sequelize = new Sequelize({
+// 	username: config.username,
+// 	password: config.password,
+// 	database: config.database,
+// 	host: config.host,
+// 	port: config.DB_PORT,
+
+// 	dialect: 'postgres',
+// 	storage: ':memory:'
+// });
